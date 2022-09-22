4d6f050730de30dab2d160eae72f39c41b5a972e
ASSASSINMAN19 <500095602@stu.upes.ac.in>
2022-09-22 16:08:33 +0530

    Basic changes

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("---------------------------------");
-        System.out.println("Welcome to Number Guessing Game!!");
+        System.out.println("Welcome to Number Guessing Game!");
         System.out.println("---------------------------------");
     }
 }
