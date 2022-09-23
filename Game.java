8c42ff0e9d6062ad02461a19805fcc2a59b1d645
Kartikay Bisaria <ktkbisaria100@gmail.com>
2022-09-23 11:20:50 +0530

    Minor Changes

--- Game.java
+++ Game.java
@@ -2,6 +2,8 @@ public class Game
 {
     public static void main(String arg[])
 	{
+        System.out.println("----------");
         System.out.println("Start game");
+        System.out.println("----------");
 	}
 }
