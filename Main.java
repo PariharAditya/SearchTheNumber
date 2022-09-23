280d0eb82d722e242060d9fd8a2a63aeac663abe
Aditya Parihar <adityparihar5050@gmail.com>
2022-09-23 10:28:29 +0530

    new

--- Main.java
+++
@@ -1,7 +0,0 @@
-class Main{
-    public static void main(String[] args) {
-        System.out.println("---------------------------------");
-        System.out.println("Welcome to Number Guessing Game!");
-        System.out.println("---------------------------------");
-    }
-}
