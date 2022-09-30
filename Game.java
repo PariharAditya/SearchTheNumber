d68cd8734aaeca8965d3bad32b9999145a243e19
Rudra <jokerfish015@gmail.com>
2022-09-30 14:00:30 +0530

    Cut-shorted the lines of code

--- Game.java
+++ Game.java
@@ -49,15 +49,10 @@ public class Game {
     {
         /* Check if inputNumber is lesser
         than secretNumber */
-        if(inputNumber < secretNumber)
+        if(inputNumber!= secretNumber)
         {
-            System.out.println(" Sorry,that is too low ");
-            numberOfTurnsLeft = numberOfTurnsLeft-1;
-            if (numberOfTurnsLeft == 0) {
-                System.out.println("YOU LOSE! The SECRET number was "+ secretNumber); 
-                playAgain();
-            }    
-            System.out.println("Try Again! Chances remaining -> "+ numberOfTurnsLeft);
+            numberOfTurnsLeft=numberOfTurnsLeft-1;
+            System.out.println("Chances remaining -> "+ numberOfTurnsLeft);
             if(secretNumber==1)
             System.out.println("The Chosen");
             else if(secretNumber==2)
@@ -73,67 +68,15 @@ public class Game {
             else if(secretNumber==7)
             System.out.println("Cristiano Ronaldo");
             else if(secretNumber==8)
-            System.out.println("The number of Hoocruxes in Harry Potter");
+            System.out.println("The number of hoocruxes in Harry Potter");
             else if(secretNumber==9)
-            System.out.println("Jewels if Akbar");
+            System.out.println("Jewels of Akbar");
             else if(secretNumber==10)
             System.out.println("The number of heads Ravana had");
             else if(secretNumber==11)
             System.out.println("Twins when you start the counting again");
             else if(secretNumber==12)
-            System.out.println("number just after twins");
-            else if(secretNumber==13)
-            System.out.println("6th prime number");
-            else if(secretNumber==14)
-            System.out.println("Valentine week");
-            else if(secretNumber==15)
-            System.out.println("Thrice of 3rd prime number");
-            else if(secretNumber==16)
-            System.out.println("Half of number of days in a month+1");
-            else if(secretNumber==17)
-            System.out.println("4th prime number + twice of five");
-            else if(secretNumber==18)
-            System.out.println("Twice of 2nd prime number X 2");
-            else if(secretNumber==19)
-            System.out.println("index is Seven in array of prime number");
-            else if(secretNumber==20)
-            System.out.println("5X2X2");
-        }
-        /* Check if inputNumber is higher
-        than secretNumber */        
-        else if(inputNumber > secretNumber)
-        {
-            System.out.println(" Sorry,that number is  too high ");
-            numberOfTurnsLeft = numberOfTurnsLeft-1;
-            if (numberOfTurnsLeft == 0) {
-                System.out.println("YOU LOSE! The SECRET number was "+ secretNumber); 
-                playAgain();
-            }
-            System.out.println("Try Again! Chances remaining ->"+ numberOfTurnsLeft);
-            if(secretNumber==1)
-            System.out.println("The Chosen");
-            else if(secretNumber==2)
-            System.out.println("Minute ki Maggi!");
-            else if(secretNumber==3)
-            System.out.println("Gandhi ji k kitne Bandar");
-            else if(secretNumber==4)
-            System.out.println("Double Date!");
-            else if(secretNumber==5)
-            System.out.println("Cost of Tapri Chai");
-            else if(secretNumber==6)
-            System.out.println("The First common multiple of starting two number");
-            else if(secretNumber==7)
-            System.out.println("Cristiano Ronaldo");
-            else if(secretNumber==8)
-            System.out.println("The Number of Hoocruxes in Harry Potter");
-            else if(secretNumber==9)
-            System.out.println("Jewels if Akbar");
-            else if(secretNumber==10)
-            System.out.println("The number of heads Ravana had");
-            else if(secretNumber==11)
-            System.out.println("one's & ten's place are same");
-            else if(secretNumber==12)
-            System.out.println("number just after twins");
+            System.out.println("Month for the homecoming of Santa Claus");
             else if(secretNumber==13)
             System.out.println("6th prime number");
             else if(secretNumber==14)
@@ -150,7 +93,26 @@ public class Game {
             System.out.println("index is 7 in array of prime number");
             else if(secretNumber==20)
             System.out.println("5X2X2");
-        }
+            if(inputNumber < secretNumber)
+            {
+                System.out.println(" Sorry,that is too low ");
+                if (numberOfTurnsLeft == 0) {
+                    System.out.println("You're out of trys! The SECRET number was "+ secretNumber); 
+                    playAgain();
+                }    
+            }
+        /* Check if inputNumber is higher
+        than secretNumber */        
+            else if(inputNumber > secretNumber)
+            {
+                System.out.println(" Sorry,that number is  too high ");
+                if (numberOfTurnsLeft == 0) {
+                    System.out.println("You're out of trys! The SECRET number was "+ secretNumber); 
+                    playAgain();
+                }
+
+            }
+        }    
         else
         {
             System.out.println("YOU WIN! The SECRET number was "+ secretNumber);
